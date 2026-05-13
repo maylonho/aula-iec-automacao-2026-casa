21646e0147ccc47f1029a98b74f6ae1833cf6d3f
maylonho <oliveira_maylon@hotmail.com>
2026-05-13 13:39:19 -0300

    first commit

---
+++ src/main/java/com/oliveira/aula_iec_automacao/AulaIecAutomacaoApplication.java
@@ -0,0 +1,13 @@
+package com.oliveira.aula_iec_automacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaIecAutomacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaIecAutomacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oliveira/aula_iec_automacao/AulaIecAutomacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveira.aula_iec_automacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaIecAutomacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
