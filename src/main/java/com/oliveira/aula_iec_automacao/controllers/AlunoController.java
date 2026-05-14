2d3dba77413596882cc90c88bb94b29f747b5cfb
maylonho <oliveira_maylon@hotmail.com>
2026-05-13 22:13:34 -0300

    add repo, model and controls for Aluno

--- src/main/java/com/oliveira/aula_iec_automacao/controllers/AlunoController.java
+++ src/main/java/com/oliveira/aula_iec_automacao/controllers/AlunoController.java
@@ -1,49 +1,47 @@
 package com.oliveira.aula_iec_automacao.controllers;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.oliveira.aula_iec_automacao.models.Aluno;
+import com.oliveira.aula_iec_automacao.repositories.AlunoRepository;
 
 @RestController
 @RequestMapping("/aluno")
 public class AlunoController {
 
     @Autowired
-    private com.oliveira.aula_iec_automacao.service.AlunoService service;
+    private AlunoRepository repository;
 
     @GetMapping
-    public List<com.oliveira.aula_iec_automacao.models.Aluno> findAll() {
-        return service.findAll();
+    public List<Aluno> findAll() {
+        return repository.findAll();
     }
 
     @GetMapping("/{id}")
     public Aluno findById(@PathVariable Long id) {
-        return service.findById(id);
+        return repository.findById(id).get();
     }
 
     @PostMapping
     public Aluno create(@RequestBody Aluno aluno) {
-        return service.create(aluno);
+        return repository.save(aluno);
     }
 
-    @PutMapping
-    public Aluno update(@RequestBody Aluno aluno) {
-        return service.update(aluno);
+    @PutMapping("/{id}")
+    public Aluno update(
+            @PathVariable Long id,
+            @RequestBody Aluno aluno) {
+
+        aluno.setId(id);
+
+        return repository.save(aluno);
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
-        service.delete(id);
+        repository.deleteById(id);
     }
 }
